84ab5a580ba1001905760569b8384098fafa0911
Richard Steven <richard.steven@hotmail.fr>
2019-03-16 10:01:42 +0100

    start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './Views/index/index.component';
+import { RegisterUserComponent } from './Views/User/register-user/register-user.component';
+import {RouterModule, Routes} from '@angular/router';
+
+
+// Routes of application
+const appRoutes: Routes = [
+    {path: '', component: IndexComponent},
+    {path: 'user/register', component: RegisterUserComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    RegisterUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
